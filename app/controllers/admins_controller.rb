1769b1821c32a0464023b58f4a9e9f19dfa588f1
Utku Demir <utdemir@gmail.com>
2013-08-28 20:03:48 +0300

    Fix: Don't redirect to User page on admins and teachers controllers

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,5 +1,5 @@
 class AdminsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_admin!
   layout 'executive'
   def welcome
   end
--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,5 +1,5 @@
 class TeachersController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_teacher!
   layout 'executive'
 
   def welcome
