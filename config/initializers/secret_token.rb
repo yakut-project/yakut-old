f5e5758aaa38f421c9994872dd630e5db1b59345
Utku Demir <utdemir@gmail.com>
2013-08-28 10:34:48 +0300

    Initialize the project (#1)
    Initialize the project and populate Gemfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Yakut::Application.config.secret_key_base = '6a7b6eaef1cbd08aaa899d64595688987e626efb9aadfa5614599ffb55a8b90e50a108040b34e06aa20887be15b22dd5ec9e8b61f135533e8af791a47e6eaa57'
