9cfd31108d24dee8cb6d26f96ab3d255a81bcf58
Ahmet Sezgin Duran <marjinalist1@gmail.com>
2013-08-28 11:50:08 +0300

    (#3) Added admin and teacher devise models

---
+++ app/models/admin.rb
@@ -0,0 +1,6 @@
+class Admin < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :timeoutable, :validatable, :lockable, :trackable
+end
