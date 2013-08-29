5ac6c7bfec23629beefb596983241f0c14a897dc
Kerem Bozdas <krmbzds.github@gmail.com>
2013-08-29 12:39:44 +0300

    Fix: Remove attribute accessors in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,4 @@ class User < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  attr_accessor :name, :surname, :webpage, :stackoverflow_page, :github_page, 
-  				:bitbucket_page, :blog_page, :twitter_page, :name, :surname, 
-  				:bio, :birthday, :gender
 end
