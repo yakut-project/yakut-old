821a12f050b859846ec676189d50da0bbf7641ee
Kerem Bozdas <krmbzds.github@gmail.com>
2013-08-30 13:11:08 +0300

    Pass size parameter to image_tag in gravatar_tag

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
   require 'digest/md5'
 
   def gravatar_tag(email, options = {})
-    size = (options.delete(:size) or "80")
+    size = (options[:size] or "80")
     default = (options.delete(:default) or "identicon")
     hash = Digest::MD5.hexdigest(email.downcase)
     image_tag "http://www.gravatar.com/avatar/#{hash}/?s=#{size}&d=#{default}", options
