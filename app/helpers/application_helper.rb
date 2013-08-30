cc8601a6354b9e9872f162a5d6784fb1a8addcca
Kerem Bozdas <krmbzds.github@gmail.com>
2013-08-30 10:33:42 +0300

    Add Gravatar integration

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  require 'digest/md5'
+
+  def gravatar_tag(email, size=80, default='identicon')
+    hash = Digest::MD5.hexdigest(email.downcase)
+    image_tag "http://www.gravatar.com/avatar/#{hash}/?s=#{size}&d=#{default}"
+  end
+
 end
