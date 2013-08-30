c4f16454d8b18fc21164c0f13f93a05a66447177
Utku Demir <utdemir@gmail.com>
2013-08-30 15:29:52 +0300

    Use a cooler db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,40 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# Must run 'rake db:seed' to create default models
-Admin.new(email: 'admin@foobar.com', password: 'password').save!
-Teacher.new(email: 'teacher@foobar.com', password: 'password').save!
-User.new(email: 'user@foobar.com', password: 'password', name: 'user', surname: 'user', gender: :not_specified, birthday: Date.today).save!
+DEFAULT_PASSWORD = 'password'
+
+admins = %w(utku kerem)
+teachers = %w(tayfun onurozgur)
+users = %w(gokhan ahmet yusuf)
+
+surnames = %w(Doe Smith Green Brown Donald McFly Jobs McManniman)
+
+admins.each do |admin| 
+	a = Admin.new
+	a.email = "#{admin}@yakut.com"
+	a.password = DEFAULT_PASSWORD
+	a.save!
+end
+
+teachers.each do |teacher|
+	t = Teacher.new
+	t.email = "#{teacher}@yakut.com"
+	t.password = DEFAULT_PASSWORD
+	t.save!
+end
+
+users.each do |user|
+	u = User.new
+	u.email = "#{user}@yakut.com"
+	u.password = DEFAULT_PASSWORD
+	u.github = user
+	u.blog = "#{user}." + ["blogspot.com", "wordpress.com"].sample
+
+	u.name = user.humanize
+	u.surname = surnames.sample
+
+	u.gender = "male"
+	u.birthday = Date.today - (rand 12*365..60*365) 
+
+	u.save!
+end
